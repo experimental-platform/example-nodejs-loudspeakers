02aa6604b67580141fb63b93c81133b221531ee1
Christopher Blum <christopher.blum@spellboy.com>
2015-09-24 15:05:10 +0200

    Add first sound

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var express = require('express')
 var app = express()
+var exec = require("child_process").exec;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+function play(fileName) {
+  exec("mplayer ./sounds/" + fileName, function(error, stdout, stderr) {
+    console.log(stdout)
+    console.log(stderr)
+  });
+}
+
+app.post("/play", function(request, response) {
+  var sound = (request.query.sound || "").replace(/[^a-z0-9_\-]/gi, "");
+  play(sound + ".mp3");
+  response.send("ok.")
 })
 
 app.listen(app.get('port'), function() {
