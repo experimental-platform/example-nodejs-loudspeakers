a6dc568ce990a83f2e08364b3c28e2dbf10c5b39
Christopher Blum <christopher.blum@spellboy.com>
2015-11-02 15:13:38 +0100

    Add README and lint js code

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# NodeJS "Hello World" Example for Experimental Platform
+# NodeJS Sound Example for Experimental Platform
 
-## Usage
+The code is easy to understand: It uses mplayer for playing a sound located in the ``sounds`` directory.
 
-    git clone git@github.com:experimental-platform/nodejs-hello-world.git
-    cd nodejs-hello-world
-    git remote add platform ssh://dokku@your-box.local:8022/nodejs-hello-world
+## Requirements
+
+* A machine that runs [Experimental Platform](https://github.com/experimental-platform/platform-configure-script)
+* A loud speaker, we used [this](http://www.amazon.com/Logitech-S150-Speakers-Digital-Sound/dp/B000ZH98LU)
+
+## Installation
+
+    git clone git@github.com:experimental-platform/example-speakers.git
+    cd example-speakers
+    git remote add platform ssh://dokku@your-box.local:8022/example-speakers
     git push platform master
--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 var exec = require("child_process").exec;
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 function play(fileName) {
   exec("mplayer ./sounds/" + fileName, function(error, stdout, stderr) {
-    console.log(stdout)
-    console.log(stderr)
+    console.log(stdout);
+    console.log(stderr);
   });
 }
 
 app.post("/play", function(request, response) {
   var sound = (request.query.sound || "").replace(/[^a-z0-9_\-]/gi, "");
   play(sound + ".mp3");
-  response.send("ok.")
-})
+  response.send("ok.");
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
